661fb905bcdd86e6d0a491694df5c4f71afc057f
bhatt325 <armaan.bhatti@mail.utoronto.ca>
2024-12-30 15:19:34 -0500

    Removed thenotes and myproject1 folders

--- thenotes/main.py
+++
@@ -1,9 +0,0 @@
-# the reason we can do this is because website is a package
-# as it has init and stuff from init can easliy be imported
-from website import create_app 
-
-app = create_app()
-
-if __name__ == "__main__": 
-    app.run(debug=True)
-     
