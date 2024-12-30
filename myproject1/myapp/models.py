aa29be8ad93fe212627c190b1df17d0f4deec66b
bhatt325 <armaan.bhatti@mail.utoronto.ca>
2024-12-30 15:04:37 -0500

    removed

--- myproject1/myapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class features(models.Model):
-    name = models.CharField(max_length=15)
-    details = models.CharField(max_length=100)
-    
